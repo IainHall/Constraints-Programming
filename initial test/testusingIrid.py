22f70494ef5af439b426b8471c86be0a5d02c7e0
IainHall <61051711+IainHall@users.noreply.github.com>
2022-06-08 12:05:42 +0100

    started trying to add a power function

--- initial test/testusingIrid.py
+++ initial test/testusingIrid.py
@@ -32,6 +32,10 @@ print(total)
 all_timeincrements = range(i+1)
 all_targets = range(j+1)
 
+
+# creates boolean array containing which satellite is being targeted and adds
+# the constraint that only one satellite may be targeted at a time
+
 targets = {}
 for ti in all_timeincrements:
     for tr in all_targets:
@@ -40,9 +44,22 @@ for ti in all_timeincrements:
 for ti in all_timeincrements:
     model.AddAtMostOne(targets[(ti,tr)] for tr in all_targets)
   
+#creating a power and energy variables
+energy = model.NewIntVar(0,24000,'Energystored')
 
+# note the power variable doesn't represent a power demand but a change in stored
+# energy for a given time increment
+power = {} 
+for ti in all_timeincrements:
+    power[(ti)] = model.NewIntVar(-25,25,'ChangeinStoredenergy')
+
+
+model.add(sum(power[(ti)]) for ti in all_timeincrements > 0)
+
+
+# adds objective to maximise the amount of targeted satellites that are also in 
+# view, i.e. to maximise the time the PBR can potentially operate
 model.Maximize(sum(targetdata[ti][tr]* targets[(ti,tr)]  for tr in all_targets for ti in all_timeincrements))
-    
 
 
 # Creates the solver and solve.
@@ -59,7 +76,7 @@ for ti in all_timeincrements:
 
 if status == cp_model.OPTIMAL:
     print('Solution:')
-    print( f'Time increments spent in operational pointing = {solver.ObjectiveValue()}', 'out of %i' %(total)  ) 
+   # print( f'Time increments spent in operational pointing = {solver.ObjectiveValue()}', 'out of %i' %(total)  ) 
     
     print('total time spent targeting a satellite = %i' % (operating_time))
     
