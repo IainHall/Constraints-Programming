0237e25866960705cf2c925d21015fa2cfcc9a05
IainHall <61051711+IainHall@users.noreply.github.com>
2022-06-10 15:14:53 +0100

    kept messing around no progress

--- initial test/testusingIrid.py
+++ initial test/testusingIrid.py
@@ -36,6 +36,10 @@ all_timeincrements = range(i+1)
 all_targets = range(j+1)
 
 
+powerCost = [-2, -4, -40]
+all_operations = range(len(powerCost))
+
+
 # creates boolean array containing which satellite is being targeted and adds
 # the constraint that only one satellite may be targeted at a time
 
@@ -51,23 +55,40 @@ for ti in all_timeincrements:
 
 for ti in all_timeincrements:
     model.AddAtMostOne(targets[(ti,tr)] for tr in all_targets)
-  
+    
+
+powerop = {}
+for ti in all_timeincrements:
+    for op in all_operations:
+       powerop= model.NewBoolVar('power op: time %i op %i' % (ti,op))
+'''
 #creating a power and energy variables
-energy = model.NewIntVar(0,24000,'Energystored')
+energy = {}
+for ti in all_timeincrements:
+    energy[ti] = model.NewIntVar(0,24000,'Energystored at time %i ' % (ti))
 
 # note the power variable doesn't represent a power demand but a change in stored
 # energy for a given time increment
 power = {} 
 for ti in all_timeincrements:
-    power[(ti)] = model.NewIntVar(-25,25,'ChangeinStoredenergy')
+        power[(ti)] = model.NewIntVar(-25,25,'ChangeinStoredenergy time%i' % (ti))
 
-
-model.Add(sum([power[ti] for ti in all_timeincrements]) > 0)
 
 for ti in all_timeincrements:
-    model.Add(power[ti] == 25).OnlyEnforceIf(eclipseData[ti] == T)
-    model.Add(power[ti] == -5).OnlyEnforceIf(eclipseData[ti] == False)
-
+    model.Add(power[ti] == )
+#model.Add(powerop[(1,0)] == 1).OnlyEnforceIf(sum(targets[(1,tr)] for tr in all_targets) > 0)
+#   model.Add(powerop[(ti,1)]==1).OnlyEnforceIf()
+#for ti in all_timeincrements:
+#    model.AddImplication(sum(targets[(ti,tr)] for tr in all_targets) > 0, energy[ti] = -2)    
+'''
+temp = {}
+    
+for ti in all_timeincrements: 
+    for op in all_operations:
+        temp[ti] = temp[ti] + powerCost[op]*powerop[ti,op]
+    
+    
+model.Add(sum(sum(powerCost[op]* powerop[ti,op]) for op in all_operations) for ti in all_timeincrements > 0)
 # adds objective to maximise the amount of targeted satellites that are also in 
 # view, i.e. to maximise the time the PBR can potentially operate
 model.Maximize(sum(targetdata[ti][tr]* targets[(ti,tr)]  for tr in all_targets for ti in all_timeincrements))
