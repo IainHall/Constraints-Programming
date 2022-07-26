06d474af101bd716755ac9cdf12ffc43d7b5ef7b
IainHall <61051711+IainHall@users.noreply.github.com>
2022-07-26 11:38:44 +0100

    fixed manual scheduler

--- manual schedule/Manual Schedule implementation.py
+++ manual schedule/Manual Schedule implementation.py
@@ -28,14 +28,14 @@ import matplotlib.pyplot as plt
 
 
 #read in the illuminator data value (avg objects detected in time increment)
-data = pd.read_csv("Avg objects Detection log.csv")
+data = pd.read_csv("../Data sets/5s, 5d, polar/Avg objects Detection log.csv ")
 target_value = data.values.tolist()
 
-data = pd.read_csv("illuminator available.csv")
+data = pd.read_csv("../Data sets/5s, 5d, polar/Illuminator view data log.csv")
 ilum_in_view = data.values.tolist()
 
 #read in the downlink data times
-data = pd.read_csv("Communications Data Log.csv")
+data = pd.read_csv("../Data sets/5s, 5d, polar/Communications Data log.csv")
 downlink_possible= data.values.tolist() 
 
 num_T = len(target_value) # final time incriment
@@ -45,53 +45,56 @@ all_sats = range(num_sats)
 all_T = range(num_T)
 all_actions = range(num_actions)
 
-action = [0 for a in all_actions for t in all_T] #4 by time length variable
-target_sat = [0 for s in all_sats for t in all_T]
+action = [[0 for a in all_actions ] for t in all_T] #4 by time length variable
+target_sat = [[0 for s in all_sats] for t in all_T]
 
 #constrained resource variables
 # note memory is in kB
 maxStorage = 64e3
 dataPerObs = 1500
+dataPerPro = 29
 
 storedData= 0
 ObsStored = 0
 Processed = 0 
 Downlinks = 0
-ObservedValue = {}
+ObservedValue = []
 ObsValIndex = 0
-ProcessedValue = {}
+ProcessedValue = []
 ProValIndex = 0
-DownlinkedValue = {}
+DownlinkedValue = []
 DownValIndex = 0
 
 # Heuristic implementation within loop
 for t in all_T:
     
-    if downlink_possible[t] == 1 and Processed > 1: # downlink decision
+    if downlink_possible[t] == 1 and Processed > 1: # downlink decision (pos 2)
         
         DownlinkedValue.append(ProcessedValue[ProValIndex])
             
-        action[2][t] = 1
+        action[t][2] = 1
         storedData = storedData - 250
         Downlinks += 1
         
-    elif ilum_in_view[t] == 1 and storedData <= maxStorage + dataPerObs:  # observe decision
-    
-        ObservedValue.append(max(target_value[s][t] for s in all_sats))
+    elif sum(ilum_in_view[t][s] for s in all_sats)== 1 and storedData <= maxStorage + dataPerObs:  # observe decision pos 0
+        
+        action[t][0] = 1
+        ObservedValue.append(max(target_value[t][s] for s in all_sats))
         storedData = storedData + dataPerObs
         ObsStored += 1
         
-    elif ObsStored > 1:      # process decision
+    elif ObsStored > 1:      # process decision  (pos 1)
     
+        action[t][1] = 1
         ProcessedValue.append(ObservedValue[ObsValIndex])
         ObsValIndex += 1
         
         ObsStored -= 1 
         Processed += 1
-        storedData = storedData - dataPerObs 
+        storedData = storedData - dataPerObs + dataPerPro
         
     else:
-        action[3][t] =1     # idle decision
+        action[t][3] =1     # idle decision pos 3
         
 
 
